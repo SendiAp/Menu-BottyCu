33ede31b1f3fd575b98257d87d3d41d0ef45850e
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-09-02 12:32:42 +0700

    Create botty.py

---
+++ botty.py
@@ -0,0 +1,78 @@
+
+import os
+import handlers
+from aiogram import executor, types
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from data import config
+from loader import dp, db, bot
+import filters
+import logging
+
+filters.setup(dp)
+
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.environ.get("PORT", 5000))
+user_message = 'Yakin , 100% Yakin'
+
+
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    markup.row(user_message)
+
+    await message.answer('''Hai Sahabat! 👋
+🤖 Saya asisten dari bot curhat botty, saya akan membantu menampilkan informasi dll.
+    
+📄 Untuk mempilkan semua menu atau informasi Kalian bisa klik /menu 
+👥 Kamu mau jadi bagian team Botty? daftarkan diri kamu menjadi admin Bottycu klik /daftar_admin
+❓ Kamu wajib join channel Botty [@BottyCu] agar mendapatkan informasi terkini dari kami.
+🤝 Ingin membuat iklan DiBot BottyCu? Hubungi pengembang <a href="https://t.me/pikyus1">Mas S</a>, dia tidak menggigit''',)
+
+@dp.message_handler(commands='hapus_tombol')
+async def cmd_tombol(message: types.Message):
+
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    markup.row(user_message)
+
+    await message.answer('''Apa Kamu Yakin Ingin Menghapus Tombolnya?''', reply_markup=markup)
+
+@dp.message_handler(text=user_message)
+async def user_mode(message: types.Message):
+    await message.answer('Ok, Saya Akan Menghapusnya.', reply_markup=ReplyKeyboardRemove())
+
+async def on_startup(dp):
+    logging.basicConfig(level=logging.INFO)
+    db.create_tables()
+
+    await bot.delete_webhook()
+    await bot.set_webhook(config.WEBHOOK_URL)
+
+
+async def on_shutdown():
+    logging.warning("Shutting down..")
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+    logging.warning("Bot down")
+
+
+if __name__ == '__main__':
+
+    if "HEROKU" in list(os.environ.keys()):
+
+        executor.start_webhook(
+            dispatcher=dp,
+            webhook_path=config.WEBHOOK_PATH,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            skip_updates=True,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
+        )
+
+    else:
+
+        executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
