52ea71c6f26f133c257bc259cfb5cb361f2ced5f
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-09-02 12:51:49 +0700

    Create markups.py

---
+++ keyboards/markups.py
@@ -0,0 +1,31 @@
+from aiogram.types import ReplyKeyboardMarkup
+
+back_message = '👈 Kembali'
+confirm_message = '✅ Konfirmasi pesanan'
+all_right_message = '✅ Betul sekali'
+cancel_message = '🚫 Membatalkan'
+
+def confirm_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(confirm_message)
+    markup.add(back_message)
+
+    return markup
+
+def back_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add(back_message)
+
+    return markup
+
+def check_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(back_message, all_right_message)
+
+    return markup
+
+def submit_markup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.row(cancel_message, all_right_message)
+
+    return markup
