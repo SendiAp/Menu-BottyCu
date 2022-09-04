27abab8a906be10cbf9d7b6680ba05446d0149f2
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-09-04 14:46:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ filters.setup(dp)
 WEBAPP_HOST = "0.0.0.0"
 WEBAPP_PORT = int(os.environ.get("PORT", 5000))
 user_message = 'Yakin , 100% Yakin'
-
+admin_message = '✅ Aktifkan Menu Admin'
 
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
@@ -25,9 +25,12 @@ async def cmd_start(message: types.Message):
     await message.answer('''Hai Sahabat! 👋
 🤖 Saya asisten dari bot curhat botty, saya akan membantu menampilkan informasi dll.
     
-📄 Untuk mempilkan semua menu atau informasi Kalian bisa klik /menu 
+📄 Untuk mempilkan semua menu atau informasi Kalian bisa klik /menu
+
 👥 Kamu mau jadi bagian team Botty? daftarkan diri kamu menjadi admin Bottycu klik /daftar_admin
+
 ❓ Kamu wajib join channel Botty [@BottyCu] agar mendapatkan informasi terkini dari kami.
+
 🤝 Ingin membuat iklan DiBot BottyCu? Hubungi pengembang <a href="https://t.me/pikyus1">Mas S</a>, dia tidak menggigit''',)
 
 @dp.message_handler(commands='hapus_tombol')
@@ -42,6 +45,24 @@ async def cmd_tombol(message: types.Message):
 @dp.message_handler(text=user_message)
 async def user_mode(message: types.Message):
     await message.answer('Ok, Saya Akan Menghapusnya.', reply_markup=ReplyKeyboardRemove())
+
+@dp.message_handler(commands='085894831504')
+async def cmd_tombol(message: types.Message):
+
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    markup.row(admin_message)
+
+    await message.answer('''❗ <b>Peringatan</b>\n\n◾<b>Perintah ini hanya digunakan oleh admin Bottycu</b> aja jika admin Botty menyebarkan Perintah ini akan dikenakan sanksi berat.''', reply_markup=markup)
+
+@dp.message_handler(text=admin_message)
+async def admin_peringatan(message: types.Message):
+
+    cid = message.chat.id
+    if cid not in config.ADMINS:
+        config.ADMINS.append(cid)
+
+    await message.answer('✅ Menu Admin Berhasil Diaktifkan.\n\n◾ Silahkan Ketik /menu untuk memunculkan Tombol.', reply_markup=ReplyKeyboardRemove())
 
 async def on_startup(dp):
     logging.basicConfig(level=logging.INFO)
